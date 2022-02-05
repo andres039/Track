60e7f2691f3f75f645c8c2e1e5653b9ae789d545
soolaf <harkysoolaf@gmail.com>
2022-02-05 19:08:00 +0100

    Create registration form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
-// import './App.css';
-
-import 'bulma/css/bulma.min.css'
+import './App.css';
+import 'bulma/css/bulma.min.css';
 import Button from './components/button';
 import Calendars from './components/calendar';
 import Navbar from './components/navbar';
+import Form from './components/registration';
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar> </Navbar>
-     <h1>Testing track, the app for musicians 🎼</h1>
-     <Button>click me</Button> 
-     <Calendars/>
+      <h1>Testing track, the app for musicians 🎼</h1>
+      <Button>click me</Button>
+      <Calendars />
+      <Form />
     </div>
   );
 }
